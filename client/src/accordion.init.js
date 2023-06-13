18600f710f6a354be8a520a51fb37d66e49c1e96
Jason Irish <jsirish@users.noreply.github.com>
2023-06-12 19:58:43 -0500

    FEATURE default bootstrap template (#42)
    * FEATURE default bootstrap template
    
    * remove thirdparty JS
    * remove client dir
    * remove JS requirements from template
    * utilize Bootstrap 4 cards and collapse

--- client/src/accordion.init.js
+++
@@ -1,8 +0,0 @@
-;(function ($) {
-    $(document).on("ready", function () {
-        var accordions = $(".accordion");
-        if (accordions.length > 0) {
-            accordions.accordion()
-        }
-    });
-})(jQuery);
